b07814b6e3a1f6594a7f5ec03986528681e1d3b0
Marius Agerup <Marius.schei@outlook.com>
2023-10-29 23:09:14 +0100

    added button with value

--- m-f-a/src/buttons/onClick.js
+++ m-f-a/src/buttons/onClick.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 function ButtonComponent() {
-  function onButtonClick() {
+  function onButtonClick(value) {
     console.log("Button is clickyTyClickingGood");
+    console.log(value);
   }
 
   return (
     <div>
       <button onClick={onButtonClick}>Click It</button>
+      <button onClick={() => onButtonClick(42)}>TheNumber</button>
     </div>
   );
 }
