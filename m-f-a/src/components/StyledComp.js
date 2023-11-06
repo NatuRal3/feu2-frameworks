f7b5530d06d15141ceec2d0b531fcb8df14d6301
Marius Agerup <Marius.schei@outlook.com>
2023-11-06 23:07:19 +0100

    added some code in StyledComp.js. It does not work, but will stand as an example anywyas.

---
+++ m-f-a/src/components/StyledComp.js
@@ -0,0 +1,20 @@
+// THIS DID NOT WORK. DOM ERROR
+
+// import React from "react";
+// import styled from "styled-components";
+
+// const Item = styled.li`
+//   background-color: ${(props) => (props.isActive ? "red" : "")};
+// `;
+
+// function StyledComponent() {
+//   return (
+//     <ul>
+//       <Item>Item 1</Item>
+//       <Item isActive={true}>Item 2</Item>
+//       <Item isActive>Item 3</Item>
+//       <Item>Item 4</Item>
+//     </ul>
+//   );
+// }
+// export default StyledComponent;
