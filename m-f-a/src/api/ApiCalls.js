a7fb426c1cd58f5ed3342eb0ceae077e9bc265b5
Marius Agerup <Marius.schei@outlook.com>
2023-12-05 23:40:31 +0100

    Added API

---
+++ m-f-a/src/api/ApiCalls.js
@@ -0,0 +1,43 @@
+import Reac, { useEffect, useState } from "react";
+
+const url = "https://api.noroff.dev/api/v1/online-shop";
+
+function ApiCalls() {
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        setIsError(false);
+        setIsLoading(true);
+        const response = await fetch(url);
+        const json = await response.json();
+        setPosts(json);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        setIsError(true);
+      }
+    }
+    getData();
+  }, []);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>Error trying to load</div>;
+  }
+  return (
+    <div>
+      {posts.map((post) => (
+        <div>
+          <h2>{post.title}</h2>
+          <p>{post.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+export default ApiCalls;
---
+++ m-f-a/src/components/NavBar.js
@@ -0,0 +1,10 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+function NavBar() {
+  return <Navbar expand="lg" className="bg-body-tertiary"></Navbar>;
+}
+
+export default NavBar;
